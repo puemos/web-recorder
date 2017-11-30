f5ac22bfe947be9bc84e39f9931e1639c2879d71
Shy <puemos@gmail.com>
2017-11-30 22:19:11 +0100

    feat: change events

--- README.md
+++ README.md
@@ -3,16 +3,51 @@
 [![styled with prettier](https://img.shields.io/badge/styled_with-prettier-ff69b4.svg)](https://github.com/prettier/prettier)
 [![Greenkeeper badge](https://badges.greenkeeper.io/puemos/web-recorder.svg)](https://greenkeeper.io/)
 [![Travis](https://img.shields.io/travis/puemos/web-recorder.svg)](https://travis-ci.org/puemos/web-recorder)
-[![Coveralls](https://img.shields.io/coveralls/puemos/web-recorder.svg)](https://coveralls.io/github/puemos/web-recorder)
 [![Dev Dependencies](https://david-dm.org/puemos/web-recorder/dev-status.svg)](https://david-dm.org/puemos/web-recorder)
 
+### Docs
+
+https://puemos.github.io/web-recorder
+
+### Example
+
+[here](https://github.com/puemos/web-recorder/blob/master/examples/simple.html)
 
 ### Usage
 
 ```js
+import { Recorder } from 'web-recorder'
 
+function onGotStream(stream) {
+  audioRecorder = new WebRecorder.Recorder(stream)
 
+  audioRecorder.addEventListener('data', ev => {
+    var blobUrl = URL.createObjectURL(ev.detail)
+
+    const record = document.getElementById('record')
+    record.src = blobUrl
+  })
+  audioRecorder.record()
+}
+
+function start() {
+  navigator.getUserMedia(
+    {
+      audio: {
+        advanced: [
+          {
+            echoCancelation: false
+          }
+        ]
+      }
+    },
+    onGotStream,
+    console.error
+  )
+}
+function stop() {
+  audioRecorder.stop()
+}
 ```
 
 ### Features
-
--- src/recorder.ts
+++ src/recorder.ts
@@ -2,56 +2,107 @@ import { EventTarget } from './EventTarget'
 
 const worker = require('worker-loader?inline=true!./workers/recorder.worker')
 
+const audioContext: AudioContext = new AudioContext()
+
 export class Recorder extends EventTarget {
   private recording: boolean
+  private ready: boolean
   private bufferLen: number
   private context: AudioContext
   private scriptNode: ScriptProcessorNode
   private worker: Worker
+  private source: GainNode
 
-  constructor(private source: GainNode) {
+  constructor(private stream: MediaStream) {
     super()
     this.recording = false
+    this.ready = false
     this.bufferLen = 4096
 
     this.onAudioProcess = this.onAudioProcess.bind(this)
     this.onWorkerMessage = this.onWorkerMessage.bind(this)
   }
-  setup() {
+
+  start() {
+    if (!this.ready) {
+      this.setup()
+    }
+    this.dispatchEvent(new CustomEvent('start'))
+    this.recording = true
+  }
+
+  pause() {
+    this.recording = false
+    this.exportWAV()
+  }
+
+  reset() {
+    this.recording = false
+    this.worker.postMessage({
+      command: 'clear'
+    })
+    this.dispatchEvent(new CustomEvent('reset'))
+  }
+
+  abort() {
+    this.kill()
+  }
+
+  getBuffer() {
+    this.worker.postMessage({ command: 'getBuffer' })
+  }
+
+  private setup() {
+    // Init the worker
     this.worker = this.worker || worker()
     this.worker.addEventListener('message', this.onWorkerMessage)
 
-    this.context = this.source.context
-    this.scriptNode = this.context.createScriptProcessor(this.bufferLen, 2, 2)
+    // Create and connect to the source
+    this.source = audioContext.createGain()
+    const audioInput = audioContext.createMediaStreamSource(this.stream)
+    const analyserNode = audioContext.createAnalyser()
+    analyserNode.fftSize = 2048
+    this.source.connect(analyserNode)
+
+    const zeroGain = audioContext.createGain()
+    zeroGain.gain.value = 0.0
+    this.source.connect(zeroGain)
+    zeroGain.connect(audioContext.destination)
+
+    this.scriptNode = audioContext.createScriptProcessor(this.bufferLen, 2, 2)
     this.source.connect(this.scriptNode)
-    this.scriptNode.connect(this.context.destination)
+    this.scriptNode.connect(audioContext.destination)
     this.scriptNode.addEventListener('audioprocess', this.onAudioProcess)
 
     this.worker.postMessage({
       command: 'init',
       payload: {
-        sampleRate: this.context.sampleRate
+        sampleRate: audioContext.sampleRate
       }
     })
-    this.dispatchEvent(new CustomEvent('started'))
+    this.ready = true
+
+    this.dispatchEvent(new CustomEvent('ready'))
   }
 
-  onWorkerMessage(ev: MessageEvent) {
+  private onWorkerMessage(ev: MessageEvent) {
     const { command, payload } = ev.data
     switch (command) {
       case 'exportWAV':
-        this.dispatchEvent(new CustomEvent('ended', { detail: payload }))
-        this.kill()
+        this.dispatchEvent(new CustomEvent('data', { detail: payload }))
         break
 
       default:
         break
     }
   }
-  onAudioProcess(ev: AudioProcessingEvent): void {
+  private onAudioProcess(ev: AudioProcessingEvent): void {
     if (!this.recording) {
       return
     }
+    this.dispatchEvent(
+      new CustomEvent('audioprocess', { detail: ev.inputBuffer })
+    )
     this.worker.postMessage({
       command: 'record',
       payload: {
@@ -63,45 +114,22 @@ export class Recorder extends EventTarget {
     })
   }
 
-  record() {
-    this.setup()
-    this.recording = true
-  }
-
-  stop() {
-    this.exportWAV()
-  }
-
-  abort() {
-    this.kill()
-    this.dispatchEvent(new CustomEvent('stopped'))
-  }
-
-  kill() {
-    this.recording = false
+  private kill() {
+    this.stream.stop()
     this.source.disconnect(this.scriptNode)
-    this.scriptNode.disconnect(this.context.destination)
+    this.scriptNode.disconnect(audioContext.destination)
     this.worker.terminate()
+    this.dispatchEvent(new CustomEvent('stop'))
   }
 
-  clear() {
-    this.worker.postMessage({
-      command: 'clear'
-    })
-  }
-
-  getBuffer() {
-    this.worker.postMessage({ command: 'getBuffer' })
-  }
-
-  exportWAV(type: string = 'audio/wav') {
+  private exportWAV(type: string = 'audio/wav') {
     this.worker.postMessage({
       command: 'exportWAV',
       payload: { type }
     })
   }
 
-  exportMonoWAV(type: string = 'audio/wav') {
+  private exportMonoWAV(type: string = 'audio/wav') {
     this.worker.postMessage({
       command: 'exportMonoWAV',
       payload: { type }
